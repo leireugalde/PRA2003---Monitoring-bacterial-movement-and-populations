d0644dfd6ee063c0aabe10357f9b95dae8993eb9
leireugalde <l.ugaldesalva@student.maastrichtuniversity.nl>
2026-09-25 17:22:52 +0200

    Update PRA2003Week3Corrected.R

--- PRA2003Week3Corrected.R
+++ PRA2003Week3Corrected.R
@@ -1,6 +1,16 @@
 #PRA2003 - Monitoring Bacterial movement and populations  
 ## Week 3 deliverable Corrections 
 ## The file PRA2003Week3 (submitted one) had errors which caused it to get incorrect results 
-## The following file is a corrected code calculating the average count of bacteria ID in different experiments
 ## This code will be used for the deliverable of Week 4 and onward
 
+## This code finds the average count of bacteria in different experiments
+## The code reads an output file, counts how many bacteria (specific ID) appear
+## It then calculates the mean count and uncertainty for each bacteria ID
+
+# How to run this script
+## Open file in VS code or R studio
+## You will be asked to type the filename (Ex. ouputSet0.txt or ouputSet1.txt)
+## Program finds the presence of the bacteria in each block
+## It then calculates its mean and standard error
+## If a bacteria is not in a block, the block is not counted into the average
+## Each bacteria ID (ex. 211), has a mutant (ex. -211)
