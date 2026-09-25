845c8ae495b808238e4236e2e2a55f73b43206a6
leireugalde <l.ugaldesalva@student.maastrichtuniversity.nl>
2026-09-25 16:49:06 +0200

    Update PRA2003Week3Corrected.R with corrections
    Corrected errors in the PRA2003Week3 code to ensure accurate results for bacterial movement and population monitoring.

--- PRA2003Week3Corrected.R
+++ PRA2003Week3Corrected.R
@@ -1,1 +1,6 @@
+#PRA2003 - Monitoring Bacterial movement and populations  
+## Week 3 deliverable Corrections 
+## The file PRA2003Week3 (submitted one) had errors which caused it to get incorrect results 
+## The following file is a corrected code calculating the average count of bacteria ID in different experiments
+## This code will be used for the deliverable of Week 4 and onward
 
